7b4f16d71cbb6fbba72a474578a092852dae2177
Jet Chung <jetchung1@gmail.com>
2020-06-28 12:36:11 -0400

    Promise I am never lettin up

---
+++ build/precache-manifest.2b0c342804a5c09d8041b75bd47bf894.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c28f0d4720006bbd5a9d54492c321632",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8765f1a01a3448104a64",
+    "url": "/static/css/main.01aea80c.chunk.css"
+  },
+  {
+    "revision": "9a073c742b5f1de7676c",
+    "url": "/static/js/2.1392d1a7.chunk.js"
+  },
+  {
+    "revision": "dcd085bcd8ad0f75d8169ed7e63cc7ab",
+    "url": "/static/js/2.1392d1a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8765f1a01a3448104a64",
+    "url": "/static/js/main.6e1fe20d.chunk.js"
+  },
+  {
+    "revision": "875a6733a564d7319c4c",
+    "url": "/static/js/runtime-main.40dbc267.js"
+  },
+  {
+    "revision": "3aa5dcbccaab655636f1682bfe2345d2",
+    "url": "/static/media/arrow-right-solid.3aa5dcbc.svg"
+  },
+  {
+    "revision": "80981f505e348bbfb5d5cabc45e431ef",
+    "url": "/static/media/bell-solid.80981f50.svg"
+  },
+  {
+    "revision": "042d1a073a66480acf66ab982deae59e",
+    "url": "/static/media/binoculars.042d1a07.svg"
+  },
+  {
+    "revision": "8bad87734ac6c834243fa4eb96cc1fe1",
+    "url": "/static/media/caret.8bad8773.svg"
+  },
+  {
+    "revision": "77ae3e33813670d137e87b51a08bd357",
+    "url": "/static/media/chart-line-solid.77ae3e33.svg"
+  },
+  {
+    "revision": "dc880f17456d5cad7f07d83554c22b9b",
+    "url": "/static/media/chevron-left-solid.dc880f17.svg"
+  },
+  {
+    "revision": "9f07a3df80b5eea9cfdcf9616e3d03d5",
+    "url": "/static/media/cog.9f07a3df.svg"
+  },
+  {
+    "revision": "08515c4f1f810bf85488d8579635377e",
+    "url": "/static/media/comments.08515c4f.svg"
+  },
+  {
+    "revision": "f9d913887fe2acb8d4aae6fda7d0ae65",
+    "url": "/static/media/dice-d20-solid.f9d91388.svg"
+  },
+  {
+    "revision": "a35f82b90ed9fba09203009c0527c964",
+    "url": "/static/media/exclamation-triangle-solid.a35f82b9.svg"
+  },
+  {
+    "revision": "d483299dce3333791512a23b9f346cbc",
+    "url": "/static/media/gavel-solid.d483299d.svg"
+  },
+  {
+    "revision": "fa2c07daf87d1ec998638f37e1412080",
+    "url": "/static/media/home-solid.fa2c07da.svg"
+  },
+  {
+    "revision": "17861ffead670d4056c589854f7e795c",
+    "url": "/static/media/i.17861ffe.svg"
+  },
+  {
+    "revision": "761f1d440200c6c828f9cee3c56d138a",
+    "url": "/static/media/meteor-solid.761f1d44.svg"
+  },
+  {
+    "revision": "192188fb7a9aff238e856a80964b5bcb",
+    "url": "/static/media/microscope-solid.192188fb.svg"
+  },
+  {
+    "revision": "5f68992b06303533ff6e1aee34c08d20",
+    "url": "/static/media/rocket-solid.5f68992b.svg"
+  },
+  {
+    "revision": "3c9152271cf963c6de24b5d595227851",
+    "url": "/static/media/school.3c915227.svg"
+  },
+  {
+    "revision": "144ff03084284c8876293946a229fd8e",
+    "url": "/static/media/sign-out.144ff030.svg"
+  },
+  {
+    "revision": "604984f96fc7e6484e61d514a82adbd0",
+    "url": "/static/media/sitemap-solid.604984f9.svg"
+  },
+  {
+    "revision": "2c5552a10c3ab0a115c9484f908f982d",
+    "url": "/static/media/times-solid.2c5552a1.svg"
+  },
+  {
+    "revision": "a3e4c2f7c32ce9a7ad6fa8d381b9d9cf",
+    "url": "/static/media/trash-solid.a3e4c2f7.svg"
+  }
+]);
